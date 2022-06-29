e4123d0502fecf7ef1ebaf98a915c74e6060a07b
sbrockmeyer <sbrockmeyer@student.neumont.edu>
2022-06-28 21:44:12 -0600

    Update app.js

--- back-end/app.js
+++ back-end/app.js
@@ -13,6 +13,20 @@ const PORT = 4000;
 
 app.use(cors());
 
+// initial loading of back-end
+app.get("/", function(req, res){
+    res.send("Hello welcome if you are looking for the api key its gonna be under '/wordsapi' ");
+});
+
+// api for the front end to connect to for the words
+app.get('/wordsapi', async function(req, res){
+    var words = await wordDatabase.DA.getAllWords();
+    console.log(`here is all of your stuff from the api`);
+    console.log(words);
+    res.json(words);
+});
+
+
 
 
 // port listenting
