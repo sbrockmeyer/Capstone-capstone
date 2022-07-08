f447bd59d15dc367d4874002d5490ffae2eadbe1
sbrockmeyer <sbrockmeyer@student.neumont.edu>
2022-07-08 12:35:32 -0600

    styling added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,18 @@
-import './App.css';
+// import './App.css';
+import './sass/style.scss';
+import { useState, useEffect } from 'react';
 import Dictionary from './components/Dictionary';
 import AllShops from './components/AllShops';
 import Login from './components/Login';
 import EditUser from './components/EditUser'
+import Home from './components/Home';
 
 function App() {
+
+
   return (
     <div className="App">
-      
+      <Home/>
     </div>
   );
 }
---
+++ front-end/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+      <h1>Welcome To *insert name of site here*</h1>
+      
+    </>
+  )
+}
+
+export default Home
