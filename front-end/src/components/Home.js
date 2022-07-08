2f27d01e9e89517653234665269ce601f593d922
sbrockmeyer <sbrockmeyer@student.neumont.edu>
2022-07-08 12:49:34 -0600

    Update Home.js

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <>
       <h1>Welcome To *insert name of site here*</h1>
-      
+      <h3>you need to signin or create an account to continue its free</h3>
     </>
   )
 }
