2b0fa697d138d7f31ff06377241f14ae62144baf
sbrockmeyer <sbrockmeyer@student.neumont.edu>
2022-07-13 14:25:21 -0600

    Update index.js

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,6 +3,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createRoot } from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
