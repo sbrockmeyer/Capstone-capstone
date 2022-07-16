ed4f8fc41e25e65f3901ce6b9567990d4d780e97
sbrockmeyer <sbrockmeyer@student.neumont.edu>
2022-07-16 11:48:55 -0600

    Update CreateShop.js

--- front-end/src/components/CreateShop.js
+++ front-end/src/components/CreateShop.js
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function CreateShop() {
+	const [ShopName, setNameShop] = useState('');
+	const [description, setDescription] = useState('');
+	const [lang, setLanguage] = useState('');
+	const [country, setCountry] = useState('');
+	const [currency, setCurrency] = useState('');
+	const [message, setMessage] = useState('');
+
+	let handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+
+			let res = await fetch(``, {
+				method: "POST",
+				body: JSON.stringify({
+					shopname: ShopName,
+					description: description,
+					lang: lang,
+					country: country,
+					currency: currency,
+				}),
+			});
+
+			let resJson = await res.json();
+
+			if (res.status === 200) {
+				setNameShop("");
+				setDescription("");
+				setDescription("");
+				setLanguage("");
+				setCountry("");
+				setCurrency("");
+				setMessage("Success shop has been made");
+			} else {
+				setMessage("something borked");
+			}
+		} catch (err) {
+			console.log(err)
+		}
+	};
+
 	return (
-		<div>CreateShop</div>
+		<>
+			<div>
+				<form onSubmit={handleSubmit}>
+					
+				</form>
+			</div>
+		</>
 	)
 }
 
