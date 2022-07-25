8987f6a4df2260e66bb3bf8c932b87e336c5c8a6
sbrockmeyer <sbrockmeyer@student.neumont.edu>
2022-07-25 12:54:25 -0600

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,44 +3,37 @@ import { useState, useEffect } from 'react';
 import Dictionary from './components/Dictionary';
 import AllShops from './components/AllShops';
 import Login from './components/Login';
-// import EditUser from './components/EditUser'
+import EditUser from './components/EditUser'
 import Home from './components/Home';
 import ShopDetails from './components/ShopDetails';
 import CreateShop from './components/CreateShop';
 import CreateUser from './components/CreateUser';
 import CreateWord from './components/CreateWord';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+
 
 function App() {
-
-  // const [login, setLoggingin] = useState([]);
-
+  const [screen, setScreen] = useState("home");
   
-
   return (
-    <Router>
+    <div>
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/logIn">LogIn</Link>
-        <Link to="/dictionary">Dictionary</Link>
+        <button onClick={() => setScreen("home")}>Home</button>
+        <button onClick={() => setScreen("create user")}>Create User</button>
+        <button onClick={() => setScreen("login")}>Login</button>
+        <button onClick={() => setScreen("dictionary")}>Dictionary</button>
       </nav>
-      <Switch>
-        <Route path="/">
-          <Home/>
-        </Route>
-        <Route path="/logIn">
-          <Login/>
-        </Route>
-        <Route path="/dictionary">
-          <Dictionary/>
-        </Route>
-      </Switch>
-    </Router>
+      {
+        screen === "home" ?
+        <Home/>
+        : screen === "create user" ?
+        <CreateUser/>
+        : screen === "login"?
+        <Login/>
+        : screen === "dictionary" ?
+        <Dictionary/>
+        : null
+      }
+    </div>
   );
 }
 
