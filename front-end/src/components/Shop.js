1878c66aefc3727bcf6dcf0a292d8532b7aa7001
sbrockmeyer <sbrockmeyer@student.neumont.edu>
2022-08-02 12:42:55 -0600

    Update Shop.js

--- front-end/src/components/Shop.js
+++ front-end/src/components/Shop.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import EditShop from './EditShop'
 
 function Shop({ shops, switchScreen, select }) {
   console.log(shops);
@@ -12,7 +13,7 @@ function Shop({ shops, switchScreen, select }) {
         <div>Language: {shops.Language}</div>
         <div>Country: {shops.Country}</div>
         <div>Currncy: {shops.Currency}</div>
-        <button onClick={() => switchScreen("update shop")}>update Shop</button>
+        <button onClick={() => switchScreen("update shop")}>update Shop<EditShop id={shops._id}/></button>
         <button>Delete Shop</button>
       </div>
     </>
