9d9f61f4a19d7edfedbe2199e41269b2eea35235
sbrockmeyer <sbrockmeyer@student.neumont.edu>
2022-08-03 17:36:44 -0600

    ahhhh

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,67 +1,51 @@
 import './sass/style.scss';
-import { useState} from 'react';
-import Dictionary from './components/Dictionary';
-import AllShops from './components/AllShops';
-import Login from './components/Login';
-import Home from './components/Home';
-import CreateShop from './components/CreateShop';
-import CreateUser from './components/CreateUser';
-import CreateWord from './components/CreateWord';
-import AllUsers from  './components/AllUsers'
-import logo from './images/Logo.png';
-import EditShop from './components/EditShop';
-import EditWord from './components/EditWord';
-import EditUser from './components/EditUser';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { useState} from 'react';
+// import Dictionary from './components/Dictionary';
+// import AllShops from './components/AllShops';
+// import Login from './components/Login';
+// import Home from './components/Home';
+// import CreateShop from './components/CreateShop';
+// import CreateUser from './components/CreateUser';
+// import CreateWord from './components/CreateWord';
+// import AllUsers from  './components/AllUsers'
+// import logo from './images/Logo.png';
+// import EditShop from './components/EditShop';
+// import EditWord from './components/EditWord';
+// import EditUser from './components/EditUser';
+import {Outlet, Link} from 'react-router-dom';
 
 
 function App() {
-  const [screen, setScreen] = useState("home");
+  // const [screen, setScreen] = useState("home");
 
-  const switchScreen = (switchScreen) => {
-    setScreen(switchScreen);
-  }
+  // const switchScreen = (switchScreen) => {
+  //   setScreen(switchScreen);
+  // }
   
   return (
-    <div>
-      {/* <br/> */}
+    <>
       <nav>
-        <img className='logo' src={logo} alt='logo'width="115" height="105"/>
-        <button className='navButton' onClick={() => setScreen("home")}>Home</button>
-        <button className='navButton' onClick={() => setScreen("create user")}>Create User</button>
-        <button className='navButton' onClick={() => setScreen("create shop")}>Create Shop</button>
-        <button className='navButton' onClick={() => setScreen("create word")}>Create Word</button>
-        <button className='navButton' onClick={() => setScreen("login")}>Login</button>
-        <button className='navButton' onClick={() => setScreen("dictionary")}>Dictionary</button>
-        <button className='navButton' onClick={() => setScreen("shops")}>See all shops</button>
-        <button className='navButton' onClick={() => setScreen("users")}>See all users</button>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/dictionary">Dictionary</Link>
+          </li>
+          <li>
+            <Link to='/allShops'>All Shops</Link>
+          </li>
+          <li>
+            <Link to='/login'>Login</Link>
+          </li>
+          <li>
+            <Link to='/AllUsers'>Users</Link>
+          </li>
+        </ul>
       </nav>
-      <br/>
-      {
-        screen === "home" ?
-        <Home/>
-        : screen === "create user" ?
-        <CreateUser/>
-        : screen === "login"?
-        <Login/>
-        : screen === "dictionary" ?
-        <Dictionary switchScreen={switchScreen}/>
-        : screen === "update word" ?
-        <EditWord/>
-        : screen === "create shop" ?
-        <CreateShop/>
-        : screen === "create word" ?
-        <CreateWord/>
-        :screen === "shops" ?
-        <AllShops switchScreen={switchScreen}/>
-        :screen === "users" ?
-        <AllUsers switchScreen={switchScreen}/>
-        :screen === "update user" ?
-        <EditUser/>
-        :screen === "update shop" ?
-        <EditShop/>
-        : null
-      }
-    </div>
+      <Outlet/>
+    </>
   );
 }
 
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -4,13 +4,28 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./components/Home";
+import Dictionary from './components/Dictionary';
+import Login from './components/Login';
+import AllShops from './components/AllShops';
+import AllUsers from './components/AllUsers';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />}>
+        <Route index element={<Home />} />
+        <Route path='/dictionary' element={<Dictionary />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/allShop' element={<AllShops/>}/>
+        <Route path='/allUsers' element={<AllUsers/>}/>
+        <Route path='*' element={<h1>404 this page doesnt exist</h1>} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
