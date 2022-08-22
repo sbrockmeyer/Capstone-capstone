86297582caa06aeabf3ece30a82155e79635f30d
sbrockmeyer <sbrockmeyer@student.neumont.edu>
2022-08-22 15:49:36 -0600

    Update Login.js

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -2,48 +2,29 @@ import React, { useState } from 'react'
 
 function Login() {
 
-  const [user, setUsername] = useState('');
-  const [pass, setPassword] = useState('');
+  const [user, setUser] = useState({
+    username:"",
+    pass:""
+  });
 
-  // const data = await response.json();
-  // if (data.status === 'yee') {
-  //   const userObject = {
-  //     id: data.userId,
-  //     username: data.username
-  //   }
-  //   window.localStorage.setItem("User", JSON.stringify(userObject));
+  const handleChange = (e) =>{
+    const {user, value} = e.target
+    setUser({
+      ...user,
+      [name]: value
+    })
+  }
 
-  //   alert('login successful');
-  //   window.location.replace("http://localhost:3000/allShops");
-  // } else {
-  //   alert('please try again')
-  // }
-  // let use = window.localStorage.getItem("User");
-  // let userInfo = JSON.parse(use);
-
-  // const logout = () => {
-  //   let use = window.localStorage.getItem("User");
-  //   window.localStorage.clear();
-  //   window.location.replace("http://localhost:3000");
-  //   console.log(use)
-  // }
-  // if (user == null) {
     return (
       <>
         <form>
-          <input type='text' value={user} placeholder='username' onChange={(e) => setUsername(e.target.value)} /><br /><br />
-          <input type='text' value={pass} placeholder='password' onChange={(e) => setPassword(e.target.value)} /><br /><br />
+          <input type='text' value={user.username} placeholder='username' onChange={handleChange} /><br /><br />
+          <input type='password' value={user.pass} placeholder='password' onChange={handleChange} /><br /><br />
           <button type='submit'>Login</button>
         </form>
       </>
     )
-  // }else{
-  //   return(
-  //     <>
-  //       <button onClick={() => logout()}>logout</button>
-  //     </>
-  //   )
-  // }
+  
 }
 
 export default Login
