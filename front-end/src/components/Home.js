70759aa539a7fe457d603dc65c9b1ed3523c9b9a
sbrockmeyer <sbrockmeyer@student.neumont.edu>
2022-08-25 14:17:34 -0600

    aaaahhh

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -4,9 +4,10 @@ import React from 'react'
 function Home() {
   return (
     <>
-
-      <h1>Welcome To Yarnaholic</h1>
-      <h3>you need to sign in or create an account to continue its free</h3>
+      <div className='title'>
+        <h1>Welcome To Yarnaholic</h1>
+        <h3>you need to sign in or create an account to continue its free</h3>
+      </div>
 
     </>
   )
